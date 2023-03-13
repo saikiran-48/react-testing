d155d5defe8a5c9564a746028c2b8f49b88bed91
Sai Kiran Kadaveru <54521490+saikiran-48@users.noreply.github.com>
2023-03-14 00:50:12 +0530

    chor : workflow

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
+import '@testing-library/jest-dom';
 
 test('greet renders correctly', () => {
   render(<Greet />);
